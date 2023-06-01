244d73d122c85b9bcd5a8bc60ba47d9de05f1ae5
Leopaski <leonardo.pascale@gmail.com>
2023-06-01 15:48:01 +0200

    immagine

--- js/myscript.js
+++ js/myscript.js
@@ -2,7 +2,18 @@ const { createApp } = Vue
     createApp({
     data() {
       return {
-        message: 'Welcome VueJs!'
+        message: 'Welcome VueJs!',
+        immagine: 'img'
       }
     }
   }).mount('#hello');
+
+  import immagine from "./img/Vue.js_Logo_2.svg.png"
+
+export default {
+  data: function () {
+    return {
+      image: immagine,
+    }
+  }
+}
